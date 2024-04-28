8e55b44ca9bd0a0c148abce23a5e430ffac33783
VioletEvergardenZz <1561862432@qq.com>
2024-04-28 22:44:04 +0800

    添加注释

--- list-files-in-folders.py
+++ list-files-in-folders.py
@@ -1,9 +1,9 @@
 import os
 def list_files_in_folders(folder_path):
-    try:
+    try:                               #异常处理的起始点
         files=os.listdir(folder_path)
         return files,None
-    except FileNotFoundError:
+    except FileNotFoundError:          #内置的异常类
         return None,"文件未发现"
     except PermissionError:
         return None,"权限拒绝"
@@ -13,7 +13,7 @@ def main():
     for folder_path in folder_paths:
         files,error_message=list_files_in_folders(folder_path)
         if files:
-            print(f"File in {folder_path}: ")
+            print(f"File in {folder_path}: ")       # f-string，可以在字符串中插入变量的值。
             for file in files:
                 print(file)
         else:
